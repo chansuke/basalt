b7f80d414afc4e9b053684e74a0ea30226598971
chansuke <chansuke@georepublic.de>
2019-08-03 00:46:36 +0900

    Add load opcode

--- src/vm.rs
+++ src/vm.rs
@@ -37,6 +37,18 @@ impl VM {
         self.counter += 1;
         return opcode;
     }
+
+    fn next_8_bits(&mut self) -> u8 {
+        let result = self.program[self.counter];
+        self.counter += 1;
+        return result;
+    }
+
+    fn next_16_bits(&mut self) -> u16 {
+        let result = ((self.program[self.counter] as u16) << 8) | self.program[self.counter + 1] as u16;
+        self.counter += 2;
+        return result;
+    }
 }
 
 #[cfg(test)]
@@ -65,5 +77,13 @@ mod tests {
         test_vm.program = test_bytes;
         test_vm.run();
         assert_eq!(test_vm.counter, 1);
+    }
+
+    #[test]
+    fn test_load_opcode() {
+        let mut test_vm = VM::new;
+        test_vm.program = vec![0, 0, 0, 1];
+        test_vm.run();
+        assert_eq!(test_vm.registers[0], 500);
     }
 }
