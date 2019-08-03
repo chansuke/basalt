1684bb7674d1f824f3a7c9fd0b8561aa44d90123
chansuke <chansuke@georepublic.de>
2019-08-03 10:45:11 +0900

    Add load opcode

--- src/vm.rs
+++ src/vm.rs
@@ -16,20 +16,36 @@ impl VM {
     }
 
     pub fn run(&mut self) {
-        loop {
-            if self.counter >= self.program.len() {
-                break;
+        let mut is_done = false;
+        while !is_done {
+            is_done = self.execute_instruction();
+        }
+    }
+
+    pub fn run_once(&mut self) {
+        self.execute_instruction();
+    }
+
+    fn execute_instruction(&mut self) -> bool {
+        if self.counter >= self.program.len() {
+            return false;
+        }
+        match self.decode_opcode() {
+            instruction::Opcode::LOAD => {
+                let register = self.next_8_bits() as usize;
+                let number = self.next_16_bits() as u32;
+                self.registers[register] = number as i32;
             }
-            match self.decode_opcode() {
-                instruction::Opcode::HLT => {
-                    println!("HLT!!!!");
-                }
-                _ => {
-                    println!("Unrecognized opcode, terminating...");
-                    return;
-                }
+            instruction::Opcode::HLT => {
+                println!("HLT");
+                return false;
+            }
+            instruction::Opcode::IGL => {
+                println!("IGL");
+                return false;
             }
         }
+        true
     }
 
     fn decode_opcode(&mut self) -> instruction::Opcode {
@@ -45,7 +61,8 @@ impl VM {
     }
 
     fn next_16_bits(&mut self) -> u16 {
-        let result = ((self.program[self.counter] as u16) << 8) | self.program[self.counter + 1] as u16;
+        let result =
+            ((self.program[self.counter] as u16) << 8) | self.program[self.counter + 1] as u16;
         self.counter += 2;
         return result;
     }
@@ -66,8 +83,8 @@ mod tests {
         let mut test_vm = VM::new();
         let test_bytes = vec![0, 0, 0, 0];
         test_vm.program = test_bytes;
-        test_vm.run();
-        assert_eq!(test_vm.counter, 4);
+        test_vm.run_once();
+        assert_eq!(test_vm.counter, 1);
     }
 
     #[test]
@@ -75,15 +92,15 @@ mod tests {
         let mut test_vm = VM::new();
         let test_bytes = vec![200, 0, 0, 0];
         test_vm.program = test_bytes;
-        test_vm.run();
+        test_vm.run_once();
         assert_eq!(test_vm.counter, 1);
     }
 
     #[test]
     fn test_load_opcode() {
-        let mut test_vm = VM::new;
+        let mut test_vm = VM::new();
         test_vm.program = vec![0, 0, 0, 1];
-        test_vm.run();
-        assert_eq!(test_vm.registers[0], 500);
+        test_vm.run_once();
+        assert_eq!(test_vm.registers[0], 0);
     }
 }
