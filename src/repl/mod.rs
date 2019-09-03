2fb4dfa245a1c4f2fad01ad6f9062ebbc3b6cfdd
chansuke <chansuke@georepublic.de>
2019-09-03 22:43:41 +0900

    Start implementing repl

---
+++ src/repl/mod.rs
@@ -0,0 +1,23 @@
+use std;
+use std::io;
+use std::io::Write;
+use vm::VM;
+
+pub struct REPL {
+    command_buffer: Vec<String>,
+    vm: VM,
+}
+
+impl REPL {
+    pub fn new() -> REPL {
+        REPL {
+            vm: VM::new(),
+            command_buffer: vec![],
+        }
+    }
+
+    pub fn run(&mut self) {
+        println!("Welcome!");
+        // WIP: loop
+    }
+}
